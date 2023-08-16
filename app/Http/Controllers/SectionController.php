6da3d3ad029ab6c629edd1bb21dedfb96a627b28
Badr Akbik <badrakbik1@gmail.com>
2023-08-16 15:21:41 +0300

    section modification

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Store\StoreSectionRequest;
+use App\Http\Requests\Update\UpdateSectionRequest;
+use App\Http\Resources\SectionResource;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class SectionController extends Controller
 {
-    //
+    public function index()
+    {
+        return SectionResource::collection(Section::all());
+    }
+
+
+    public function store(StoreSectionRequest $request)
+    {
+        return new SectionResource(Section::create($request->safe()->all()));
+    }
+
+
+    public function show(Section $section)
+    {
+        return new SectionResource($section);
+    }
+
+
+    public function update(UpdateSectionRequest $request, Section $section)
+    {
+        $section->update($request->safe()->all());
+        return new SectionResource($section);
+    }
+
+
+    public function destroy(Section $section)
+    {
+        $section->delete();
+
+        return response()->json([
+            'data' => 'This Section has been deleted successfuly'
+        ]);
+    }
 }
