fe626e15ffefff0ea52ca568c7dd50b3d8a0f43d
Badr Akbik <badrakbik1@gmail.com>
2023-08-16 13:00:15 +0300

    Mother CRUD

--- app/Http/Controllers/MotherController.php
+++ app/Http/Controllers/MotherController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Store\StoreMotherRequest;
+use App\Http\Requests\Update\UpdateMotherRequest;
+use App\Http\Resources\MotherResource;
+use App\Models\Mother;
 
 class MotherController extends Controller
 {
-    //
+    public function index()
+    {
+        return MotherResource::collection(Mother::all());
+    }
+
+
+    public function store(StoreMotherRequest $request)
+    {
+        return new MotherResource(Mother::create($request->safe()->all()));
+    }
+
+
+    public function show(Mother $attendance)
+    {
+        return new MotherResource($attendance);
+    }
+
+
+    public function update(UpdateMotherRequest $request, Mother $attendance)
+    {
+        $attendance->update($request->safe()->all());
+        return new MotherResource($attendance);
+    }
+
+
+    public function destroy(Mother $attendance)
+    {
+        $attendance->delete();
+
+        return response()->json([
+            'data' => 'This Mother has been deleted successfuly'
+        ]);
+    }
 }
--- app/Http/Requests/Update/UpdateMotherRequest.php
+++ app/Http/Requests/Update/UpdateMotherRequest.php
@@ -11,7 +11,7 @@ class UpdateMotherRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateMotherRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['string', 'max:30'],
+            'phone_num' => ['numeric', 'max:15'],
+            'work' => ['string', 'max:30'],
+            'date_of_birth' => ['date']
         ];
     }
 }
