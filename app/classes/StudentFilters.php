130ee076b0a77ba0f42d108e0c6e1b648b1c7e46
Badr Akbik <badrakbik1@gmail.com>
2023-08-31 17:21:30 +0300

    student queries

--- app/classes/StudentFilters.php
+++ app/classes/StudentFilters.php
@@ -4,9 +4,9 @@ namespace App\Classes;
 
 class StudentFilters extends QueryFilter
 {
-    public function search($search)
+    public function name($name)
     {
-        return $this->query->where('name', 'like', '%' . $search . '%');
+        return $this->query->where('name', 'like', '%' . $name . '%');
     }
 
     public function grade($grade)
@@ -17,5 +17,62 @@ class StudentFilters extends QueryFilter
     public function section($section)
     {
         return $this->query->whereRelation('section', 'name', $section);
+    }
+
+    public function failure($subjectCount)
+    {
+        return $this->query->whereHas('results', function ($query) {
+            $query->whereHas(
+                'subject',
+                fn ($query) =>
+                $query->whereColumn('results.mark', '<', 'subjects.min_mark')
+            );
+        }, '>=', $subjectCount);
+    }
+
+    public function absence($absenceCount)
+    {
+        return $this->query->whereRelation('attendances', 'status', false, '>=', $absenceCount);
+    }
+
+    public function mother($name)
+    {
+        return $this->query->whereHas(
+            'mother',
+            fn ($query) =>
+            $query->where('name', 'like', '%' . $name . '%')
+        );
+    }
+
+    public function father($name)
+    {
+        return $this->query->whereHas(
+            'father',
+            fn ($query) =>
+            $query->where('name', 'like', '%' . $name . '%')
+        );
+    }
+
+    public function hasRemainingPayment()
+    {
+        return $this->query->whereHas(
+            'accountments',
+            fn ($query) =>
+            $query->whereColumn('payments', '<', 'total')
+        );
+    }
+
+    public function payed($amount)
+    {
+        return $this->query->whereRelation('accountments', 'payments', '>=', $amount);
+    }
+
+    public function outcome($subject)
+    {
+        return $this->query->whereHas(
+            'outcomes',
+            fn ($query) =>
+            $query->whereHas('subject', fn ($query) => $query->where('name', $subject))
+        );
     }
 }
